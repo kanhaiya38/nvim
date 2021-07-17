c9cd059f534b44de1931b7647a763ce6cfe2bd1a
Kanhaiya M <kanhaiya38@protonmail.com>
2021-07-17 19:29:21 +0530

    feat: add window keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,6 +7,11 @@ map('i', 'jj', '<Esc>', opts)
 map('i', 'jk', '<Esc>', opts)
 map('i', 'kj', '<Esc>', opts)
 
+map('n', '<C-J>', '<C-w>j', opts)
+map('n', '<C-K>', '<C-w>k', opts)
+map('n', '<C-H>', '<C-w>h', opts)
+map('n', '<C-L>', '<C-w>l', opts)
+
 opts = { noremap = true, silent = true }
 map('n', '<C-n>', ':lua require("plugins.configs.nvim-tree").toggle()<CR>', opts)
 map('n', '<leader>r', ':NvimTreeRefresh<CR>', opts)

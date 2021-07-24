7acb9cce06368d4d460ec94f4a44f17cce11ad11
Kanhaiya M <kanhaiya38@protonmail.com>
2021-07-24 22:49:11 +0530

    feat: setup formatter for python and rust

--- lua/plugins/configs/formatter.lua
+++ lua/plugins/configs/formatter.lua
@@ -4,8 +4,22 @@ require('formatter').setup {
     javascript = {
       function()
         return {
-          exe = 'prettier',
-          args = { '--stdin-filepath', vim.api.nvim_buf_get_name(0), '--single-quote' },
+          exe = './node_modules/.bin/prettier',
+          args = { '--stdin-filepath', vim.api.nvim_buf_get_name(0) },
+          stdin = true,
+        }
+      end,
+    },
+    python = {
+      function()
+        return { exe = 'black', args = { '-' }, stdin = true }
+      end,
+    },
+    rust = {
+      function()
+        return {
+          exe = 'rustfmt',
+          args = { '--emit=stdout' },
           stdin = true,
         }
       end,
@@ -16,6 +30,16 @@ require('formatter').setup {
           exe = 'stylua',
           args = {},
           stdin = false,
+        }
+      end,
+    },
+    cpp = {
+      function()
+        return {
+          exe = 'clang-format',
+          args = {},
+          stdin = true,
+          cwd = vim.fn.expand '%:p:h', -- Run clang-format in cwd of the file.
         }
       end,
     },

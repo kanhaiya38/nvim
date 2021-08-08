b38d342355ac8359f0f554e4d4d4e49588504c8e
Kanhaiya M <kanhaiya38@protonmail.com>
2021-08-09 01:26:21 +0530

    fix: configur lsp with custom defaults

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,60 +1,27 @@
--- keymaps
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
-  local function buf_set_option(...)
-    vim.api.nvim_buf_set_option(bufnr, ...)
-  end
-
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  local opts = { noremap = true, silent = true }
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'gh', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-
-  -- Disable language server formatting
-  client.resolved_capabilities.document_formatting = false;
-
-  -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
-    augroup lsp_document_highlight
-    autocmd! * <buffer>
-    autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-    autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-    augroup END
-    ]],
-      false
-    )
-  end
-end
+local utils = require 'lsp.utils'
 
 -- config that activates keymaps and enables snippet support
-local function make_config()
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
-  capabilities.textDocument.completion.completionItem.snippetSupport = true
-  return {
-    -- enable snippet support
-    capabilities = capabilities,
-    -- map buffer local keybindings when the language server attaches
-    on_attach = on_attach,
+local function make_config(server)
+  local default_config = {
+    capabilities = require('lsp.utils').default_capabilities(),
+    settings = nil,
+    on_attach = require('lsp.utils').default_on_attach,
   }
+
+  if server == nil then
+    server = default_config
+  end
+  if server.capabilities == nil then
+    server.capabilities = default_config.capabilities
+  end
+  if server.settings == nil then
+    server.settings = default_config.settings
+  end
+  if server.on_attach == nil then
+    server.on_attach = default_config.on_attach
+  end
+
+  return server
 end
 
 -- lsp-install
@@ -66,8 +33,11 @@ local function setup_servers()
   -- add manually installed servers
   table.insert(servers, 'clangd')
 
+  local server_configs = {}
+  server_configs.lua = require 'lsp.servers.lua'
+
   for _, server in pairs(servers) do
-    local config = make_config()
+    local config = make_config(server_configs[server])
     require('lspconfig')[server].setup(config)
   end
 end
@@ -80,5 +50,5 @@ require('lspinstall').post_install_hook = function()
   vim.cmd 'bufdo e'
 end
 
-require('lsp.utils').show_source()
-require('lsp.utils').diagnostics_symbols()
+utils.show_source()
+utils.diagnostics_symbols()

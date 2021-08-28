7f67aca963f33ba7d918928cb4214324e90d9c06
Kanhaiya M <kanhaiya38@protonmail.com>
2021-08-28 19:22:50 +0530

    feat: configure different formatters and linters null-ls

--- lua/plugins/configs/null-ls.lua
+++ lua/plugins/configs/null-ls.lua
@@ -1,7 +1,36 @@
-require('null-ls').config {
-  sources = {
-    require('null-ls').builtins.formatting.stylua,
-    require('null-ls').builtins.formatting.prettier,
-  },
+local formatting = require 'null-ls.builtins.formatting'
+local code_actions = require 'null-ls.builtins.code-actions'
+local diagnostics = require 'null-ls.builtins.diagnostics'
+
+local sources = {
+  -- lua
+  formatting.stylua,
+  diagnostics.luacheck,
+
+  -- js
+  formatting.prettier,
+
+  -- python
+  formatting.black,
+  diagnostics.flake8,
+  formatting.isort,
+
+  -- c/c++
+  formatting.clang_format,
+
+  -- markdown
+  diagnostics.markdownlint,
+
+  -- shell scripts
+  formatting.shfmt,
+  diagnostics.shellcheck,
+  formatting.fish_indent,
+
+  -- rust
+  formatting.rustfmt,
+
+  -- git
+  code_actions.gitsigns,
 }
+require('null-ls').config { sources = sources }
 require('lspconfig')['null-ls'].setup {}

bd6caa83238528bdf0d0f979eaa4b422e29b0e8b
Kanhaiya M <kanhaiya38@protonmail.com>
2021-09-06 00:23:45 +0530

    feat: configure snippets

--- lua/plugins/configs/snippets.lua
+++ lua/plugins/configs/snippets.lua
@@ -1,3 +1,14 @@
 require('luasnip/loaders/from_vscode').lazy_load {
   paths = { '~/.local/share/nvim/site/pack/packer/start/friendly-snippets' },
 }
+
+local ls = require 'luasnip'
+
+local cpp = {}
+for key, val in pairs(require 'snippets.cpp') do
+  table.insert(cpp, ls.parser.parse_snippet(key, val))
+end
+
+require('luasnip').snippets = {
+  cpp = cpp,
+}

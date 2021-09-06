e04f9cb66456334fbd4cb8087eb5bd314778d8a9
Kanhaiya M <kanhaiya38@protonmail.com>
2021-09-07 00:16:48 +0530

    chore: remove commented code

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -10,7 +10,6 @@ parser_configs.norg = {
 }
 require('nvim-treesitter.configs').setup {
   ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-  -- ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension

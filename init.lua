37a8d88655754c8e3bf7903e8c0b07e8ad4a5aae
Kanhaiya M <kanhaiya38@protonmail.com>
2021-12-16 23:20:32 +0530

    fix: impatient error on fresh install

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
-require 'impatient'.enable_profile()
+local status_ok, impatient = pcall(require, "impatient")
+if status_ok then
+	impatient.enable_profile()
+end
 require 'settings'
 require 'keymaps'
 require 'plugins'

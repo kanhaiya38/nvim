8bc0005c6ed5205e5563469531154ff54a536eef
Kanhaiya M <kanhaiya38@protonmail.com>
2022-02-15 00:34:01 +0530

    fix: don't lazyload telescope plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -95,7 +95,7 @@ return require('packer').startup {
         require('configs.telescope').setup()
       end,
     }
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', after = 'telescope.nvim' }
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
     use {
       'folke/trouble.nvim',

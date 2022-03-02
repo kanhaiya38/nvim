3e7ba773683f5d486640b456dd004d770ae44967
Kanhaiya M <kanhaiya38@protonmail.com>
2022-03-03 03:43:22 +0530

    feat: add treesitter parser for neorg tables and document.meta tag

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -1,6 +1,24 @@
 -- for neorg
 local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
 
+-- These two are optional and provide syntax highlighting
+-- for Neorg tables and the @document.meta tag
+parser_configs.norg_meta = {
+  install_info = {
+    url = 'https://github.com/nvim-neorg/tree-sitter-norg-meta',
+    files = { 'src/parser.c' },
+    branch = 'main',
+  },
+}
+
+parser_configs.norg_table = {
+  install_info = {
+    url = 'https://github.com/nvim-neorg/tree-sitter-norg-table',
+    files = { 'src/parser.c' },
+    branch = 'main',
+  },
+}
+
 require('nvim-treesitter.configs').setup {
   ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ignore_install = {}, -- List of parsers to ignore installing

23b1b0748d833cc4eafcb232f1b65ee372ad72cd
Kanhaiya M <kanhaiya38@protonmail.com>
2022-03-04 03:22:46 +0530

    fix: lsp_signature not working when lazyloaded

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -72,7 +72,6 @@ return require('packer').startup {
     }
     use {
       'ray-x/lsp_signature.nvim',
-      event = 'InsertEnter',
       config = function()
         require('lsp_signature').setup()
       end,

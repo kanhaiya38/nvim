dc93240dc6cee0921077a3ce4597c4946b1d98e2
Kanhaiya M <kanhaiya38@protonmail.com>
2022-05-05 19:02:18 +0530

    fix: nvim-treesitter ensure_installed = 'maintained' deprecated

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -20,7 +20,7 @@ parser_configs.norg_table = {
 }
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = 'all', -- A list of parser names, or "all"
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension

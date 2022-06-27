d17cba4526ebce73248a561a9402d174402ca588
Kanhaiya M <kanhaiya38@protonmail.com>
2022-06-27 23:45:19 +0530

    chore: move telescope.lua to telescope/init.lua

--- lua/configs/telescope/init.lua
+++ lua/configs/telescope/init.lua
@@ -16,6 +16,11 @@ M.setup = function()
         },
       },
     },
+    pickers = {
+      find_files = {
+        theme = 'dropdown',
+      },
+    },
     extensions = {
       fzf = {
         fuzzy = true, -- false will only do exact matching
@@ -29,6 +34,14 @@ M.setup = function()
 
   telescope.load_extension 'fzf'
   telescope.load_extension 'projects'
+end
+
+M.project_files = function()
+  local opts = {} -- define here if you want to define something
+  local ok = pcall(require('telescope.builtin').git_files, opts)
+  if not ok then
+    require('telescope.builtin').find_files(opts)
+  end
 end
 
 return M

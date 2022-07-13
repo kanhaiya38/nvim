257a0bfb15f14e83e1ffde01115c2965c45a7f30
Kanhaiya M <kanhaiya38@protonmail.com>
2022-07-13 11:50:29 +0530

    fix: comments in jsx

--- lua/configs/comment.lua
+++ lua/configs/comment.lua
@@ -1,28 +1,33 @@
+local contains = function(list, value)
+  for _, v in pairs(list) do
+    if v == value then
+      return true
+    end
+  end
+end
+
 require('Comment').setup {
-  -- NOTE: The example below is a proper integration and it is RECOMMENDED.
-  {
-    ---@param ctx Ctx
-    pre_hook = function(ctx)
-      -- Only calculate commentstring for tsx filetypes
-      if vim.bo.filetype == 'typescriptreact' then
-        local U = require 'Comment.utils'
+  pre_hook = function(ctx)
+    local fts = { 'typescriptreact', 'typescript', 'javascriptreact', 'javascript' }
+    -- Only calculate commentstring for tsx filetypes
+    if contains(fts, vim.bo.filetype) then
+      local U = require 'Comment.utils'
 
-        -- Detemine whether to use linewise or blockwise commentstring
-        local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
+      -- Detemine whether to use linewise or blockwise commentstring
+      local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
 
-        -- Determine the location where to calculate commentstring from
-        local location = nil
-        if ctx.ctype == U.ctype.block then
-          location = require('ts_context_commentstring.utils').get_cursor_location()
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-          location = require('ts_context_commentstring.utils').get_visual_start_location()
-        end
-
-        return require('ts_context_commentstring.internal').calculate_commentstring {
-          key = type,
-          location = location,
-        }
+      -- Determine the location where to calculate commentstring from
+      local location = nil
+      if ctx.ctype == U.ctype.block then
+        location = require('ts_context_commentstring.utils').get_cursor_location()
+      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+        location = require('ts_context_commentstring.utils').get_visual_start_location()
       end
-    end,
-  },
+
+      return require('ts_context_commentstring.internal').calculate_commentstring {
+        key = type,
+        location = location,
+      }
+    end
+  end,
 }

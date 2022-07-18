2d8ac02c0d73de74a8cade11be2ea5f955e6952c
Kanhaiya M <kanhaiya38@protonmail.com>
2022-07-18 15:35:41 +0530

    refactor: use which-key for settings keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,25 +1,36 @@
-local map = vim.keymap.set
+local utils = require 'utils'
 
 -- Windows
-map('n', '<C-q>', '<Cmd>Bdelete<CR>')
-map({ 'n', 'i', 't' }, '<C-j>', '<Esc><Cmd>wincmd j<CR>')
-map({ 'n', 'i', 't' }, '<C-k>', '<Esc><Cmd>wincmd k<CR>')
-map({ 'n', 'i', 't' }, '<C-h>', '<Esc><Cmd>wincmd h<CR>')
-map({ 'n', 'i', 't' }, '<C-l>', '<Esc><Cmd>wincmd l<CR>')
+local windows = {
+  ['<c-j>'] = { '<esc><cmd>wincmd j<cr>', 'down' },
+  ['<c-k>'] = { '<esc><cmd>wincmd k<cr>', 'up' },
+  ['<c-h>'] = { '<esc><cmd>wincmd h<cr>', 'left' },
+  ['<c-l>'] = { '<esc><cmd>wincmd l<cr>', 'right' },
+}
 
-map({ 'n', 'i', 't' }, '<C-n>', '<Esc><Cmd>NvimTreeToggle<CR>')
+local buffer = {
+  ['<c-q>'] = { '<cmd>Bdelete<cr>', 'delete buffer' },
+  ['<C-y>'] = { 'ggVG"+y', 'copy buffer' },
+}
 
--- Lspsaga
--- map('n', '<C-j>', '<cmd>lua require("lspsaga.action").smart_scroll_with_saga(1)<CR>', opts)
--- map('n', '<C-k>', '<cmd>lua require("lspsaga.action").smart_scroll_with_saga(-1)<CR>', opts)
-
-local opts = { silent = true, nowait = true }
+-- NvimTree
+local nvim_tree = {
+  ['<c-n>'] = { '<esc><cmd>NvimTreeToggle<cr>', 'tree toggle' },
+}
 
 -- ToggleTerm
-map('n', '<C-Space>', '<Cmd>ToggleTerm<CR>', opts)
+local toggleterm = {
+  ['<C-Space>'] = { '<Cmd>ToggleTerm<CR>', 'open terminal' },
+}
 
-map('n', '<C-y>', 'ggVG"+y', opts)
-map('n', '<leader>d', 'm`:silent +g/\\m^\\s*$/d<CR>``:noh<CR>', opts)
-map('n', '<leader>D', 'm`:silent -g/\\m^\\s*$/d<CR>``:noh<CR>', opts)
-map('n', '<leader>o', '<Cmd>set paste<CR>m`o<Esc>``:set nopaste<CR>', opts)
-map('n', '<leader>O', '<Cmd>set paste<CR>m`O<Esc>``:set nopaste<CR>', opts)
+local opts = { nowait = true }
+
+utils.set_keymaps(windows, opts, { 'n', 'i', 't' })
+utils.set_keymaps(buffer, opts, { 'n', 'i', 't' })
+utils.set_keymaps(nvim_tree, opts, { 'n', 'i', 't' })
+utils.set_keymaps(toggleterm, opts)
+
+-- map('n', '<leader>d', 'm`:silent +g/\\m^\\s*$/d<CR>``:noh<CR>', opts)
+-- map('n', '<leader>D', 'm`:silent -g/\\m^\\s*$/d<CR>``:noh<CR>', opts)
+-- map('n', '<leader>o', '<Cmd>set paste<CR>m`o<Esc>``:set nopaste<CR>', opts)
+-- map('n', '<leader>O', '<Cmd>set paste<CR>m`O<Esc>``:set nopaste<CR>', opts)
---
+++ lua/utils.lua
@@ -0,0 +1,31 @@
+local wk = require 'which-key'
+
+local M = {}
+
+-- defaults:
+-- {
+--   mode = "n",
+--   prefix = "",
+--   buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+--   silent = true, -- use `silent` when creating keymaps
+--   noremap = true, -- use `noremap` when creating keymaps
+--   nowait = false, -- use `nowait` when creating keymaps
+-- }
+M.set_keymaps = function(mappings, opts, mode)
+  if type(mappings) ~= 'table' then
+    vim.notify_once('[utils.set_keymaps] Invalid mappings ' .. (mappings or 'nil'), vim.log.levels.ERROR)
+    return
+  end
+
+  if type(mode) == 'table' then
+    for _, mode_val in ipairs(mode) do
+      M.set_keymaps(mappings, opts, mode_val)
+    end
+    return
+  end
+
+  opts = vim.tbl_deep_extend('force', opts or {}, { mode = mode })
+  wk.register(mappings, opts)
+end
+
+return M

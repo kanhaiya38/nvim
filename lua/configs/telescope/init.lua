85f70fed39b11752524ee7075331a6e993f60d3d
Kanhaiya M <kanhaiya38@protonmail.com>
2022-08-13 16:20:14 +0530

    refactor: telescope config

--- lua/configs/telescope/init.lua
+++ lua/configs/telescope/init.lua
@@ -1,6 +1,18 @@
 local actions = require('telescope.actions')
 local telescope = require('telescope')
-local extensions= require('configs.telescope.extensions')
+
+local fzf = {
+  fuzzy = true, -- false will only do exact matching
+  override_generic_sorter = true, -- override the generic sorter
+  override_file_sorter = true, -- override the file sorter
+  case_mode = 'smart_case', -- or "ignore_case" or "respect_case"
+  -- the default case_mode is "smart_case"
+}
+
+local load_extensions = function()
+  telescope.load_extension('fzf')
+  telescope.load_extension('projects')
+end
 
 telescope.setup({
   defaults = {
@@ -27,7 +39,9 @@ telescope.setup({
       theme = 'dropdown',
     },
   },
-  extensions = extensions.config,
+  extensions = {
+    fzf = fzf,
+  },
 })
 
-extensions.setup();
+load_extensions()

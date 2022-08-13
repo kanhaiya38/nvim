ebb18703319422e0af34d4c16c42a496614376fc
Kanhaiya M <kanhaiya38@protonmail.com>
2022-08-14 03:41:59 +0530

    refactor: toggleterm config, remove unused config

--- lua/configs/toggleterm.lua
+++ lua/configs/toggleterm.lua
@@ -1,71 +1,12 @@
-vim.o.hidden = true -- otherwise terminal will be discarded when closed
-
-local M = {}
-M.terminals = {}
-
-M.setup = function()
-  require('toggleterm').setup({
-    open_mapping = [[<C-Space>]],
-    direction = 'vertical',
-    persist_size = false,
-    size = function(term)
-      if term.direction == 'horizontal' then
-        return 15
-      elseif term.direction == 'vertical' then
-        return vim.o.columns * 0.4
-      end
-    end,
-  })
-  local Terminal = require('toggleterm.terminal').Terminal
-  M.terminals.lazygit = Terminal:new({
-    cmd = 'lazygit',
-    hidden = true,
-    direction = 'float',
-    float_opts = {
-      border = 'curved',
-      width = 120,
-      height = 33,
-      highlights = {
-        border = 'TelescopeBorder',
-        background = 'TelescopeNormal',
-      },
-    },
-    on_open = function(term)
-      vim.api.nvim_buf_set_keymap(
-        term.bufnr,
-        'i',
-        '<C-c>',
-        '<cmd>require("configs.toggleterm").toggle("lazygit")<CR>',
-        { noremap = true, silent = true }
-      )
-    end,
-  })
-end
-
-M.toggle = function(terminal)
-  M.terminals[terminal]:toggle()
-end
-
-M.exec = function(cmd)
-  require('toggleterm.terminal').Terminal
-    :new({
-      cmd = cmd,
-      close_on_exit = false,
-      direction = 'float',
-      float_opts = {
-        border = 'curved',
-        width = 120,
-        height = 33,
-        highlights = {
-          border = 'TelescopeBorder',
-          background = 'TelescopeNormal',
-        },
-      },
-      on_open = function(term)
-        vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
-      end,
-    })
-    :toggle()
-end
-
-return M
+require('toggleterm').setup({
+  open_mapping = [[<C-Space>]],
+  direction = 'vertical',
+  persist_size = false,
+  size = function(term)
+    if term.direction == 'horizontal' then
+      return 15
+    elseif term.direction == 'vertical' then
+      return vim.o.columns * 0.4
+    end
+  end,
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -129,7 +129,7 @@ return require('packer').startup({
         require('keymaps').toggleterm()
       end,
       config = function()
-        require('configs.toggleterm').setup()
+        require('configs.toggleterm')
       end,
     })
 

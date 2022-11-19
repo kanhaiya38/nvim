24857a3becf6434d71b437bee76d8d1f8e3a4671
Kanhaiya M <kanhaiya38@protonmail.com>
2022-11-19 18:31:29 +0530

    feat: use pre_hook by ts-context-commentstring

--- lua/configs/comment.lua
+++ lua/configs/comment.lua
@@ -1,38 +1,8 @@
 local M = {}
 
-local contains = function(list, value)
-  for _, v in pairs(list) do
-    if v == value then
-      return true
-    end
-  end
-end
-
 M.setup = function()
   require('Comment').setup({
-    pre_hook = function(ctx)
-      local fts = { 'typescriptreact', 'typescript', 'javascriptreact', 'javascript' }
-      -- Only calculate commentstring for tsx filetypes
-      if contains(fts, vim.bo.filetype) then
-        local U = require('Comment.utils')
-
-        -- Detemine whether to use linewise or blockwise commentstring
-        local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
-
-        -- Determine the location where to calculate commentstring from
-        local location = nil
-        if ctx.ctype == U.ctype.block then
-          location = require('ts_context_commentstring.utils').get_cursor_location()
-        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-          location = require('ts_context_commentstring.utils').get_visual_start_location()
-        end
-
-        return require('ts_context_commentstring.internal').calculate_commentstring({
-          key = type,
-          location = location,
-        })
-      end
-    end,
+    pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
   })
 end
 

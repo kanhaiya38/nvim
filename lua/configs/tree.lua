14e2a32c6a262134fe6f00a03ae088f8957b0989
Kanhaiya M <kanhaiya38@protonmail.com>
2023-02-02 18:33:47 +0530

    feat: typescript lsp file rename with neo-tree

--- lua/configs/tree.lua
+++ lua/configs/tree.lua
@@ -1,5 +1,27 @@
 local M = {}
 M.setup = function()
+  local events = require('neo-tree.events')
+
+  ---@class FileMovedArgs
+  ---@field source string
+  ---@field destination string
+
+  ---@param args FileMovedArgs
+  local function on_file_remove(args)
+    local ts_clients = vim.lsp.get_active_clients({ name = 'tsserver' })
+    for _, ts_client in ipairs(ts_clients) do
+      ts_client.request('workspace/executeCommand', {
+        command = '_typescript.applyRenameFile',
+        arguments = {
+          {
+            sourceUri = vim.uri_from_fname(args.source),
+            targetUri = vim.uri_from_fname(args.destination),
+          },
+        },
+      })
+    end
+  end
+
   require('neo-tree').setup({
     close_if_last_window = true,
     window = {
@@ -25,6 +47,16 @@ M.setup = function()
           ['l'] = 'open',
           ['h'] = 'close_node',
         },
+      },
+    },
+    event_handlers = {
+      {
+        event = events.FILE_MOVED,
+        handler = on_file_remove,
+      },
+      {
+        event = events.FILE_RENAMED,
+        handler = on_file_remove,
       },
     },
   })

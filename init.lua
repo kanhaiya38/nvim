5d183e460853f46beae417564403a305522f8dad
Kanhaiya M <kanhaiya38@protonmail.com>
2023-02-11 23:34:23 +0530

    chore: remove impatient from init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
-local status_ok, impatient = pcall(require, 'impatient')
-if status_ok then
-  impatient.enable_profile()
-end
 require('settings').setup()
 require('keymaps').defaults()
 require('main').setup()

5ee6514fbb8a41c7c00c19bd48edc0c475cd7843
Kanhaiya M <kanhaiya38@protonmail.com>
2023-02-11 12:33:51 +0530

    feat: snippets typescript

--- snippets/typescript.lua
+++ snippets/typescript.lua
@@ -6,6 +6,15 @@ local export_default_as = {
   t("';"),
 }
 
+local console_log = {
+  t("console.log('"),
+  l(l._1, { 1 }),
+  t("', "),
+  i(1, 'val'),
+  t(');'),
+}
+
 return {
   s('eda', export_default_as),
+  s('clo', console_log),
 }

58250217c879e36a8b66d33364512164bafa4018
Kanhaiya M <kanhaiya38@protonmail.com>
2023-03-29 12:09:11 +0530

    feat: typescript snippets

--- snippets/typescript.lua
+++ snippets/typescript.lua
@@ -6,6 +6,28 @@ local export_default_as = {
   t("';"),
 }
 
+local export_all_as = {
+  t('export * as '),
+  l(l._1, { 1 }),
+  t(" from './"),
+  i(1, ''),
+  t("'"),
+}
+
+local import_all_as = {
+  t('import * as '),
+  l(l._1:gsub('%.%./', ''):gsub('^%./', ''):gsub('^@', ''), { 1 }),
+  t(" from '"),
+  i(1, ''),
+  t("'"),
+}
+
+local export_all_from = {
+  t("export * from './"),
+  i(1, ''),
+  t("'"),
+}
+
 local console_log = {
   t("console.log('"),
   l(l._1, { 1 }),
@@ -14,7 +36,28 @@ local console_log = {
   t(');'),
 }
 
+local here = {
+  t("console.log('here');"),
+}
+
+local af = {
+  t('async function '),
+  i(1, 'name'),
+  t('(params: '),
+  i(2, 'params'),
+  t('): Promise<'),
+  i(3, 'return'),
+  t('> {'),
+  i(0),
+  t('}'),
+}
+
 return {
   s('eda', export_default_as),
+  s('eas', export_all_as),
+  s('eaf', export_all_from),
+  s('ias', import_all_as),
   s('clo', console_log),
+  s('here', here),
+  s('af', af),
 }

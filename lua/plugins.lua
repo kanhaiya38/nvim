5ebdea8943940c36a31385d8ac8d6f223710ed38
Kanhaiya M <kanhaiya38@protonmail.com>
2023-03-29 12:07:20 +0530

    feat: lazy load comments.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -67,7 +67,7 @@ local plugins = {
   -- Comments
   {
     'numToStr/Comment.nvim',
-    lazy = false,
+    keys = { 'gcc', 'gcj', 'gck', { 'gc', mode = 'v' } },
     config = function()
       require('Comment').setup({
         pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),

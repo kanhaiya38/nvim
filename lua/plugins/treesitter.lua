f9fc391d394091d3d382cbdf723738b7a6606312
Kanhaiya M <kanhaiya38@protonmail.com>
2023-04-29 03:09:05 +0530

    feat: add treesj.nvim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -121,6 +121,16 @@ local plugins = {
       },
     },
   },
+  {
+  {
+    'Wansmer/treesj',
+    keys = { '<space>m', '<space>j', '<space>s' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('treesj').setup({--[[ your config ]]
+      })
+    end,
+  },
 }
 
 return plugins

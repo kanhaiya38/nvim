e2869d3c016a9dd693885ffa528498fd95a2ed09
Kanhaiya M <kanhaiya38@protonmail.com>
2023-04-29 03:17:06 +0530

    fix: treesj config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -122,14 +122,10 @@ local plugins = {
     },
   },
   {
-  {
     'Wansmer/treesj',
     keys = { '<space>m', '<space>j', '<space>s' },
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    config = function()
-      require('treesj').setup({--[[ your config ]]
-      })
-    end,
+    config = true,
   },
 }
 

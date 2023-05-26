4e8181a2c909ee4e05be040ec4e07c036148ecbb
Kanhaiya M <kanhaiya38@protonmail.com>
2023-05-26 14:21:24 +0530

    feat: add treesitter playground

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -82,6 +82,25 @@ local textobjects = {
   },
 }
 
+local playground = {
+  enable = true,
+  disable = {},
+  updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+  persist_queries = false, -- Whether the query persists across vim sessions
+  keybindings = {
+    toggle_query_editor = 'o',
+    toggle_hl_groups = 'i',
+    toggle_injected_languages = 't',
+    toggle_anonymous_nodes = 'a',
+    toggle_language_display = 'I',
+    focus_language = 'f',
+    unfocus_language = 'F',
+    update = 'R',
+    goto_node = '<cr>',
+    show_help = '?',
+  },
+}
+
 ---@type LazySpec
 local plugins = {
   {
@@ -100,6 +119,7 @@ local plugins = {
         context_commentstring = context_commentstring,
         rainbow = rainbow,
         textobjects = textobjects,
+        playground = playground,
       })
     end,
     build = ':TSUpdate',
@@ -121,6 +141,7 @@ local plugins = {
       },
     },
   },
+  { 'nvim-treesitter/playground', cmd = 'TSPlaygroundToggle' },
   {
     'Wansmer/treesj',
     keys = { '<space>m', '<space>j', '<space>s' },

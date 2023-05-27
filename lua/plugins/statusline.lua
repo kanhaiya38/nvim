eb755eb34995e4774c653c0ba12d0e696ab3d845
Kanhaiya M <kanhaiya38@protonmail.com>
2023-05-27 20:40:52 +0530

    feat: statusline improvements

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,38 +1,25 @@
 local conditions = {
-  buffer_not_empty = function()
-    return vim.fn.empty(vim.fn.expand('%:t')) ~= 1
-  end,
   hide_in_width = function()
     return vim.fn.winwidth(0) > 80
-  end,
-  check_git_workspace = function()
-    local filepath = vim.fn.expand('%:p:h')
-    local gitdir = vim.fn.finddir('.git', filepath .. ';')
-    return gitdir and #gitdir > 0 and #gitdir < #filepath
   end,
 }
 
 local config = function()
-  -- Eviline config for lualine
-  -- Author: shadmansaleh
-  -- Credit: glepnir
   local colors = require('onedark.colors')
   local icons = require('icons')
-  -- Config
+
   local separator1 = {
     function()
       return icons.misc.Separator
     end,
-    color = { fg = colors.blue }, -- Sets highlighting of component
-    padding = { left = 0, right = 1 }, -- We don't need space before this
+    color = { fg = colors.blue },
+    padding = { left = 0, right = 1 },
   }
   local mode = {
-    -- mode component
     function()
       return ''
     end,
     color = function()
-      -- auto change color according to neovims mode
       local mode_color = {
         n = colors.red,
         i = colors.green,
@@ -60,13 +47,9 @@ local config = function()
     padding = { right = 1 },
   }
 
-  local filename = {
-    'filename',
-    cond = conditions.buffer_not_empty,
-    color = { fg = colors.purple, gui = 'bold' },
-  }
+  local filename = { 'filename' }
   local location = { 'location' }
-  local file_progress = { 'progress', color = { fg = colors.fg, gui = 'bold' } }
+  local file_progress = { 'progress' }
   local diagnostics = {
     'diagnostics',
     sources = { 'nvim_diagnostic' },
@@ -74,11 +57,6 @@ local config = function()
       error = icons.diagnostic.Error,
       warn = icons.diagnostic.Warning,
       info = icons.diagnostic.Info,
-    },
-    diagnostics_color = {
-      color_error = { fg = colors.red },
-      color_warn = { fg = colors.yellow },
-      color_info = { fg = colors.cyan },
     },
   }
 
@@ -108,7 +86,6 @@ local config = function()
   }
   local diff = {
     'diff',
-    -- Is it me or the symbol for modified us really weird
     symbols = {
       added = icons.git.GitAdded,
       modified = icons.git.GitModified,
@@ -128,22 +105,24 @@ local config = function()
     color = { fg = colors.blue },
     padding = { left = 1 },
   }
+  local updates = {
+    require('lazy.status').updates,
+    cond = require('lazy.status').has_updates,
+    color = { fg = '#ff9e64' },
+  }
 
   local opts = {
     options = {
-      -- Disable sections and component separators
       component_separators = '',
       section_separators = '',
-      globalstatus = true, -- enable global statusline (have a single statusline
+      globalstatus = true,
       theme = 'onedark',
     },
     sections = {
-      -- these are to remove the defaults
       lualine_a = {},
       lualine_b = {},
       lualine_y = {},
       lualine_z = {},
-      -- These will be filled later
       lualine_c = {
         separator1,
         mode,
@@ -152,20 +131,16 @@ local config = function()
       },
       lualine_x = {
         lsp,
-        {
-          function()
-            return '%='
-          end,
-        },
+        '%=',
         diagnostics,
         filename,
         location,
+        updates,
         file_progress,
         separator2,
       },
     },
     inactive_sections = {
-      -- these are to remove the defaults
       lualine_a = {},
       lualine_b = {},
       lualine_y = {},
@@ -183,6 +158,9 @@ local plugins = {
   {
     'nvim-lualine/lualine.nvim',
     lazy = false,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
     config = config,
   },
 }

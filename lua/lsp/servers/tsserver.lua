76d8c554319be2c4d14271fbce68363462b4eb43
Kanhaiya M <kanhaiya38@protonmail.com>
2023-07-24 15:59:22 +0530

    feat: enable inlay hints for tsserver

--- lua/lsp/servers/tsserver.lua
+++ lua/lsp/servers/tsserver.lua
@@ -4,8 +4,35 @@ M.setup = function(server_opts)
   local on_attach = server_opts.on_attach
   server_opts.on_attach = function(client, bufnr)
     on_attach(client, bufnr)
+    vim.lsp.inlay_hint(bufnr, true)
     require('keymaps').typescript(bufnr)
   end
+
+  server_opts.settings = {
+    javascript = {
+      inlayHints = {
+        includeInlayEnumMemberValueHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
+        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayVariableTypeHints = true,
+      },
+    },
+    typescript = {
+      inlayHints = {
+        includeInlayEnumMemberValueHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
+        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayVariableTypeHints = true,
+      },
+    },
+  }
+
   require('typescript').setup({
     disable_commands = false, -- prevent the plugin from creating Vim commands
     debug = false, -- enable debug logging for commands

b10382c4cedbf739c30766c49fa4892de7b8287b
Kanhaiya M <kanhaiya38@protonmail.com>
2023-10-22 19:39:49 +0530

    fix: disable inlay hints

--- lua/lsp/servers/tsserver.lua
+++ lua/lsp/servers/tsserver.lua
@@ -4,7 +4,7 @@ M.setup = function(server_opts)
   local on_attach = server_opts.on_attach
   server_opts.on_attach = function(client, bufnr)
     on_attach(client, bufnr)
-    vim.lsp.inlay_hint(bufnr, true)
+    -- vim.lsp.inlay_hint(bufnr, true)
     require('keymaps').typescript(bufnr)
   end
 

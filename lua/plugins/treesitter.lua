5f7d5fbe51ed89c189ef59a27e9dadd57b26aab2
Kanhaiya M <kanhaiya38@protonmail.com>
2023-10-22 19:39:27 +0530

    fix: better treesitter context options

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -126,7 +126,18 @@ local plugins = {
     lazy = false,
     dependencies = 'nvim-treesitter/nvim-treesitter',
     config = function()
-      require('treesitter-context').setup()
+      require('treesitter-context').setup({
+        max_lines = 5,
+        -- min_window_height = 30,
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+      })
     end,
   },
   {

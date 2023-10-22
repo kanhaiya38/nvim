505088e6afaebd4f3794da59974f14cfd6e77879
Kanhaiya M <kanhaiya38@protonmail.com>
2023-10-22 19:41:43 +0530

    fix: luasnip use js snippets for ts files

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -2,6 +2,8 @@ local config = function()
   require('luasnip.loaders.from_vscode').lazy_load()
 
   require('luasnip.loaders.from_lua').load({ paths = '~/.config/nvim/snippets' })
+
+  require('luasnip').filetype_extend('typescript', { 'javascript' })
 end
 
 ---@type LazySpec
